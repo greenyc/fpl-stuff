1577a944a63b3bcfdceec8c693a7b4892107537f
Ciaran Green <ciarantgreen@gmail.com>
2024-08-01 11:49:35 +0100

    No dist in git

--- dist/consts.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BOOTSTRAP_URL = exports.FIXTURES_URL = void 0;
-exports.FIXTURES_URL = "https://fantasy.premierleague.com/api/fixtures/?future=1";
-exports.BOOTSTRAP_URL = "https://fantasy.premierleague.com/api/bootstrap-static/";
-//# sourceMappingURL=consts.js.map
